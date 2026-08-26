03b3b920e00e4f4b838e7f53994752a50b2e2f2f
Fija-khan <fijapathan9763@gmail.com>
2026-08-26 18:01:35 +0530

    backend: configure media file handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,6 +139,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+
+
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
@@ -166,3 +175,5 @@ REST_FRAMEWORK = {
 
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
